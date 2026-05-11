914191ce4ee948fdc45ffa735fc4d73b56b50857
SINING <sittn40@gmail.com>
2026-05-11 12:38:18 +0800

    Practical Submission

--- models/students.js
+++ models/students.js
@@ -2,10 +2,12 @@ const { query } = require('../database');
 const { SQL_ERROR_CODE, UNIQUE_VIOLATION_ERROR, RAISE_EXCEPTION } = require('../errors');
 
 module.exports.retrieveAll = function retrieveAll() {
-    const sql = `SELECT adm_no, stud_name, gender, crse_code FROM student`;
-    return query(sql).then(function (result) {
-        return result.rows;
-    });
+// retrieve students via stored procedure
+const sql = `SELECT adm_no, stud_name, gender, crse_code, gpa, gpa_last_updated
+FROM student`;
+return query(sql).then(function (result) {
+return result.rows;
+});
 };
 
 module.exports.enrolNewStudent = function enrolNewStudent(adminNumber, studentName, gender, address, dob, nationality, courseCode) {
--- routes/modules.js
+++ routes/modules.js
@@ -1,8 +1,8 @@
 // See https://expressjs.com/en/guide/routing.html for routing
 
-const express = require('express');
-const modulesController = require('../controllers/modulesController');
-const jwtMiddleware = require('../middleware/jwtMiddleware');
+const express = require("express");
+const modulesController = require("../controllers/modulesController");
+const jwtMiddleware = require("../middleware/jwtMiddleware");
 
 const router = express.Router();
 
@@ -11,16 +11,57 @@ const router = express.Router();
 // But you can also apply the jwtMiddleware to individual routes
 router.use(jwtMiddleware.verifyToken, jwtMiddleware.verifyIsAdmin);
 
-router.post('/', modulesController.create);
+router.post("/", modulesController.create);
 
-router.get('/:code', modulesController.retrieveByCode);
+router.get("/:code", modulesController.retrieveByCode);
 
-router.delete('/:code', modulesController.deleteByCode);
+router.delete("/:code", modulesController.deleteByCode);
 
-router.put('/:code', modulesController.updateByCode);
+router.put("/:code", modulesController.updateByCode);
 
-router.get('/', modulesController.retrieveAll);
+router.get("/", modulesController.retrieveAll);
 
-router.post('/table', modulesController.initTable);
+router.post("/table", modulesController.initTable);
+// Create a new module
+module.exports.create = function create(code, name, credit) {
+  return query("CALL create_module($1, $2, $3)", [code, name, credit])
+    .then(function (result) {
+      console.log("Module created successfully");
+    })
+    .catch(function (error) {
+      throw error;
+    });
+};
+//Update a module by code
+module.updateModule = (modCode, modName, modCredit) => {
+  const sql = "CALL updateModuleProc(?, ?, ?)";
+  dbConn.query(sql, [modCode, modName, modCredit]);
+};
+
+module.exports.deleteByCode = function deleteByCode(code) {
+  return query("CALL delete_module($1)", [code])
+    .then(function (result) {
+      console.log("Module deleted successfully");
+    })
+    .catch(function (error) {
+      throw error;
+    });
+};
+
+
+// Delete a module by code
+// router.post("/deleteModule", (req, res) => {
+//   const modCode = req.body.mod_code;
+
+//   modules.deleteModule(modCode, (error, result) => {
+//     if (error) {
+//       req.flash("error", error.message);
+//       return res.redirect("/modules");
+//     }
+
+//     req.flash("success", "Module deleted successfully!");
+//     res.redirect("/modules");
+//   });
+// });
 
 module.exports = router;
